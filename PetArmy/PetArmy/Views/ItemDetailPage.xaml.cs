7c751d70409eb043e82fb25c7106c01fee3d8ed1
CHAVEZ RIVAS <45377464+jesse0099@users.noreply.github.com>
2022-05-30 19:19:06 -0600

    Commit Inicial: Plantilla default para proyecto con Xamarin Forms. Android: 12 S, iOS: Default

---
+++ PetArmy/PetArmy/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PetArmy.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace PetArmy.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ PetArmy/PetArmy/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using PetArmy.Models;
+using PetArmy.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PetArmy.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
