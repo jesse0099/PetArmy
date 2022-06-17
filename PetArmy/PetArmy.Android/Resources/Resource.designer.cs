0557a3364d64c37f93e58c513421c6851dafc9cf
LinuxPingu <47802332+LinuxPingu@users.noreply.github.com>
2022-06-17 12:49:44 -0600

    Login Re-work

--- PetArmy/PetArmy.Android/Resources/Resource.designer.cs
+++ PetArmy/PetArmy.Android/Resources/Resource.designer.cs
@@ -17326,61 +17326,64 @@ namespace PetArmy.Droid
 			public const int notify_panel_notification_icon_bg = 2131165400;
 			
 			// aapt resource value: 0x7F0700D9
-			public const int popup_forms_android_close_icon = 2131165401;
+			public const int PetsArmy = 2131165401;
 			
 			// aapt resource value: 0x7F0700DA
-			public const int popup_forms_android_shadow = 2131165402;
+			public const int popup_forms_android_close_icon = 2131165402;
 			
 			// aapt resource value: 0x7F0700DB
-			public const int popup_forms_dark_theme_close_icon = 2131165403;
+			public const int popup_forms_android_shadow = 2131165403;
 			
 			// aapt resource value: 0x7F0700DC
-			public const int popup_forms_light_theme_close_icon = 2131165404;
+			public const int popup_forms_dark_theme_close_icon = 2131165404;
 			
 			// aapt resource value: 0x7F0700DD
-			public const int preference_list_divider_material = 2131165405;
+			public const int popup_forms_light_theme_close_icon = 2131165405;
 			
 			// aapt resource value: 0x7F0700DE
-			public const int sfcheckbox_animation = 2131165406;
+			public const int preference_list_divider_material = 2131165406;
 			
 			// aapt resource value: 0x7F0700DF
-			public const int sfcheckbox_checked = 2131165407;
+			public const int sfcheckbox_animation = 2131165407;
 			
 			// aapt resource value: 0x7F0700E0
-			public const int sfcheckbox_checked_indeterminate = 2131165408;
+			public const int sfcheckbox_checked = 2131165408;
 			
 			// aapt resource value: 0x7F0700E1
-			public const int sfcheckbox_checked_unchecked = 2131165409;
+			public const int sfcheckbox_checked_indeterminate = 2131165409;
 			
 			// aapt resource value: 0x7F0700E2
-			public const int sfcheckbox_indeterminate = 2131165410;
+			public const int sfcheckbox_checked_unchecked = 2131165410;
 			
 			// aapt resource value: 0x7F0700E3
-			public const int sfcheckbox_indeterminate_checked = 2131165411;
+			public const int sfcheckbox_indeterminate = 2131165411;
 			
 			// aapt resource value: 0x7F0700E4
-			public const int sfcheckbox_indeterminate_unchecked = 2131165412;
+			public const int sfcheckbox_indeterminate_checked = 2131165412;
 			
 			// aapt resource value: 0x7F0700E5
-			public const int sfcheckbox_unchecked = 2131165413;
+			public const int sfcheckbox_indeterminate_unchecked = 2131165413;
 			
 			// aapt resource value: 0x7F0700E6
-			public const int sfcheckbox_unchecked_checked = 2131165414;
+			public const int sfcheckbox_unchecked = 2131165414;
 			
 			// aapt resource value: 0x7F0700E7
-			public const int sfcheckbox_unchecked_indeterminate = 2131165415;
+			public const int sfcheckbox_unchecked_checked = 2131165415;
 			
 			// aapt resource value: 0x7F0700E8
-			public const int test_custom_background = 2131165416;
+			public const int sfcheckbox_unchecked_indeterminate = 2131165416;
 			
 			// aapt resource value: 0x7F0700E9
-			public const int tooltip_frame_dark = 2131165417;
+			public const int test_custom_background = 2131165417;
 			
 			// aapt resource value: 0x7F0700EA
-			public const int tooltip_frame_light = 2131165418;
+			public const int tooltip_frame_dark = 2131165418;
 			
 			// aapt resource value: 0x7F0700EB
-			public const int xamarin_logo = 2131165419;
+			public const int tooltip_frame_light = 2131165419;
+			
+			// aapt resource value: 0x7F0700EC
+			public const int xamarin_logo = 2131165420;
 			
 			static Drawable()
 			{
