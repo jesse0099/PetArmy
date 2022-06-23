36253902a32c8508dcf67e55f0fb71e656b94dff
CHAVEZ RIVAS <45377464+jesse0099@users.noreply.github.com>
2022-06-22 18:55:09 -0600

    Status bar color para modo claro

--- PetArmy/PetArmy.Android/MainActivity.cs
+++ PetArmy/PetArmy.Android/MainActivity.cs
@@ -58,7 +58,7 @@ namespace PetArmy.Droid
                     }
                 case Xamarin.Forms.OSAppTheme.Light:
                     {
-                        Xamarin.Forms.Application.Current.Resources.TryGetValue("JacksonPurple", out output);
+                        Xamarin.Forms.Application.Current.Resources.TryGetValue("LuckyPoint", out output);
                         Window.SetStatusBarColor(Android.Graphics.Color.ParseColor(((Xamarin.Forms.Color)output).ToHex()));
                         break;
                     }
