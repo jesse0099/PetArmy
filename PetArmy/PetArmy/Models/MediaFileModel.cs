746166d85536beb966eca787621ae58af3287835
CHAVEZ RIVAS <45377464+jesse0099@users.noreply.github.com>
2022-07-09 01:18:37 -0600

    Exorcismo - Fix para problemas de binding para listview de refugios

--- PetArmy/PetArmy/Models/MediaFileModel.cs
+++ PetArmy/PetArmy/Models/MediaFileModel.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
 using System.Text;
 using Xamarin.Forms;
-using System.Drawing.Imaging;
 
 namespace PetArmy.Models
 {
