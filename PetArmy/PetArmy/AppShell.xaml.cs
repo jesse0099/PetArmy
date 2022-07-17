e2d19d04dc6afb53b25306d4c61df7a27c457dd5
CHAVEZ RIVAS <45377464+jesse0099@users.noreply.github.com>
2022-07-17 15:30:21 -0600

    App.xaml AppShell.xaml.cs

--- PetArmy/PetArmy/AppShell.xaml.cs
+++ PetArmy/PetArmy/AppShell.xaml.cs
@@ -1,4 +1,5 @@
-﻿using PetArmy.Interfaces;
+﻿using PetArmy.Infraestructure;
+using PetArmy.Interfaces;
 using PetArmy.Views;
 using System;
 using Xamarin.Forms;
@@ -8,7 +9,7 @@ namespace PetArmy
     public partial class AppShell : Xamarin.Forms.Shell
     {
         IFirebaseAuth _i_auth;
-         
+
 
         public AppShell()
         {
@@ -21,9 +22,14 @@ namespace PetArmy
         {
             _i_auth = DependencyService.Get<IFirebaseAuth>();
             if (_i_auth.SignOut())
+            {
+                object output;
+                Xamarin.Forms.Application.Current.Resources.TryGetValue("Locator", out output);
+                ((InstanceLocator)output).Main.Login.RefreshUiState();
                 await Shell.Current.GoToAsync("//LoginPage");
+            }
             else
-                await DisplayAlert("Error","Error",null);
+                await DisplayAlert("Error", "Error", null);
         }
     }
 }
