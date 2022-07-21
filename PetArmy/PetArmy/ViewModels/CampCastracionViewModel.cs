b9e10aef6cc39e9a518e69319610add9fb198f13
gabo19-01 <gabrielgranadosc@gmail.com>
2022-07-20 19:55:53 -0600

    Continuación Tarea Campañas
    Se crea ViewModel de CampCastra.
    Se crea View de AddCampCastra y CampCastra.

---
+++ PetArmy/PetArmy/ViewModels/CampCastracionViewModel.cs
@@ -0,0 +1,69 @@
+﻿using PetArmy.Services;
+using PetArmy.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PetArmy.ViewModels
+{
+    public class CampCastracionViewModel : BaseViewModel
+    {
+
+        private static CampCastracionViewModel _instance;
+        public static CampCastracionViewModel GetInstance()
+        {
+            if (_instance == null)
+                return new CampCastracionViewModel();
+            else
+                return _instance;
+
+        }
+
+        public CampCastracionViewModel()
+        {
+            initCommands();
+            initClass();
+
+        }
+
+        public void initClass()
+        {
+
+        }
+
+        public ICommand getAllCampCastra { get; set; }
+
+        public ICommand AddCampCastra { get; set; }
+
+        public void initCommands()
+        {
+            getAllCampCastra = new Command(getCampCastra);
+            AddCampCastra = new Command(addCampCastra);
+        }
+
+        public async void getCampCastra()
+        {
+            try
+            {
+                await GraphQLService.getAllCampCastra();
+            }
+            catch
+            {
+            }
+        }
+
+        public async void addCampCastra()
+        {
+            try
+            {
+                await Application.Current.MainPage.Navigation.PushAsync(new AddCampCastraView());
+            }
+            catch (System.Exception e)
+            {
+                await App.Current.MainPage.DisplayAlert("Couldn't open 'Add Campaña Castración'", e.ToString(), "Ok");
+            }
+        }
+    }
+}
